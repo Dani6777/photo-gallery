95e597271835b8edc7a8f4645d4b344f5880c8b6
Daniel Morales <daniel.morales23@outlook.cl>
2024-04-20 17:01:14 -0400

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
+    defineCustomElements(window);
     <App />
   </React.StrictMode>
 );
